fe997f9407707ef3916e8b265022bdf1d6562cfb
luisBaena92 <72351222+luisBaena92@users.noreply.github.com>
2020-10-06 14:40:07 -0500

    articulos gratis y maquetacion vitrina productos

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -30,6 +30,9 @@
 
 	?>
 
+	<!--=====================================
+		PLUGINS CSS
+	======================================-->
 	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plugins/bootstrap.min.css">
 
 	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plugins/font-awesome.min.css">
@@ -38,11 +41,20 @@
 
 	<link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed" rel="stylesheet">
 
+	<!--=====================================
+		HOJAS DE ESTILO PERSONALIZADAS
+	======================================-->
+
 	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/plantilla.css">
 
 	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/cabezote.css">
 
 	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/slide.css">
+	<link rel="stylesheet" href="<?php echo $url; ?>vistas/css/productos.css">
+
+	<!--=====================================
+		PLUGINS JAVASCRIPT
+	======================================-->
 
 	<script src="<?php echo $url; ?>vistas/js/plugins/jquery.min.js"></script>
 
@@ -121,11 +133,15 @@ if(isset($_GET["ruta"])){
 }else{
 
 	include "modulos/slide.php";
+	include "modulos/destacados.php";
+
 
 }
 
 ?>
-
+<!--=====================================
+		JAVA SCRIPT PERSONALIZADO
+	======================================-->
 <script src="<?php echo $url; ?>vistas/js/cabezote.js"></script>
 <script src="<?php echo $url; ?>vistas/js/plantilla.js"></script>
 <script src="<?php echo $url; ?>vistas/js/slide.js"></script>
